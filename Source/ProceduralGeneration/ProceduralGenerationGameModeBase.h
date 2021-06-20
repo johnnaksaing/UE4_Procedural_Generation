4d8af329cb67794ee29a21fba16997e800b3ba9a
johnnaksaing <johnnaksaing@daum.net>
2021-06-20 13:55:45 +0900

    초기 커밋 메시지입니다.

---
+++ Source/ProceduralGeneration/ProceduralGenerationGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProceduralGenerationGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROCEDURALGENERATION_API AProceduralGenerationGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
